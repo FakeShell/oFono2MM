6c51df18bf746aaae6a92a6e8baac93f4646ffa0
Bardia Moshiri <fakeshell@bardia.tech>
2024-07-21 13:37:33 -0400

    logging: try using voicecall object path if modem_name is not available
    useful when we get messages from mm_call
    
    Signed-off-by: Bardia Moshiri <fakeshell@bardia.tech>

--- ofono2mm/logging.py
+++ ofono2mm/logging.py
@@ -10,7 +10,14 @@ def ofono2mm_print(message, verbose):
     if 'self' in caller_frame.f_locals:
         cls = caller_frame.f_locals['self']
         cls_name = cls.__class__.__name__
-        obj_path = getattr(cls, 'modem_name', 'Unknown') if hasattr(cls, 'modem_name') else 'Unknown'
+
+        if hasattr(cls, 'modem_name'):
+            obj_path = getattr(cls, 'modem_name', 'Unknown')
+        elif hasattr(cls, 'voicecall'):
+            obj_path = getattr(cls, 'voicecall', 'Unknown')
+        else:
+            obj_path = 'Unknown'
+
         func_name = caller_frame.f_code.co_name
         full_message = f"{cls_name}({obj_path}).{func_name}: {message}"
     else:
